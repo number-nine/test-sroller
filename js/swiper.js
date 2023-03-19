94bb7c206e61bf3dd92c36f2e34310058d33dc37
number-nine <sergii.pokhylko@gmail.com>
2023-03-19 10:27:43 +0200

    multiSwiper

--- js/swiper.js
+++ js/swiper.js
@@ -1,9 +1,9 @@
-const swiper = new Swiper(".swiper", {
+const swiper1 = new Swiper(".swiper1", {
   spaceBetween: 16,
   slidesPerView: 1,
 
   breakpoints: {
-    
+
     // when window width is >= px
     768: {
       spaceBetween: 16,
@@ -17,7 +17,34 @@ const swiper = new Swiper(".swiper", {
 
   // If we need pagination
   pagination: {
-    el: ".swiper-pagination",
+    el: ".swiper-pagination1",
   },
 
 });
+
+
+
+const swiper2 = new Swiper(".swiper2", {
+  spaceBetween: 16,
+  slidesPerView: 2,
+
+  breakpoints: {
+
+    // when window width is >= px
+    768: {
+      spaceBetween: 16,
+      slidesPerView: 3,
+    },
+    1200: {
+      spaceBetween: 28,
+      slidesPerView: 4,
+    },
+  },
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination2",
+  },
+
+});
+
