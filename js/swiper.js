19ea670235b3557361a223caae0e505c5f034979
number-nine <sergii.pokhylko@gmail.com>
2023-03-19 02:26:57 +0200

    media and slidesPerView

--- js/swiper.js
+++ js/swiper.js
@@ -1,12 +1,23 @@
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper(".swiper", {
+  spaceBetween: 16,
+  slidesPerView: 1,
 
-    spaceBetween: 28,
-    slidesPerView: 'auto',
+  breakpoints: {
+    
+    // when window width is >= px
+    768: {
+      spaceBetween: 16,
+      slidesPerView: 2,
+    },
+    1200: {
+      spaceBetween: 28,
+      slidesPerView: 3,
+    },
+  },
 
   // If we need pagination
   pagination: {
-    el: '.swiper-pagination',
+    el: ".swiper-pagination",
   },
-
 
 });
